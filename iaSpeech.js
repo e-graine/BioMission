2ba2492db1b9694d6604750dcd688cc233678775
Yan Callier <yan.callier@gmail.com>
2020-07-27 09:49:01 +0200

    test intro crash 5

--- iaSpeech.js
+++ iaSpeech.js
@@ -1,5 +1,19 @@
-var introSpeech = "Le joueur découvre l’univers dans lequel il est plongé :  Une intelligence artificielle d’aide à la décision  est élaborée par les pouvoirs publics locaux. A 1/2 heure de son activation, l’I.A prends l’initiative d’étendre ses connaissances en s’adressant à un public peu mis à contribution dans la recherche de solution : les moins de 18 ans."
-var welcomeSpeech = 'Welcome, Human...'
+// var introSpeech = {
+//     name: 'introSpeech',
+//     texts : [
+//         "Le joueur découvre l’univers dans lequel il est plongé :",
+//         "Une intelligence artificielle d’aide à la décision  est élaborée par les pouvoirs publics locaux.",
+//         "A 1/2 heure de son activation, l’I.A prends l’initiative d’étendre ses connaissances",
+//         "en s’adressant à un public peu mis à contribution dans la recherche de solution : les moins de 18 ans."
+//     ]
+// }
+var introSpeech = [
+    "Le joueur découvre l’univers dans lequel il est plongé :",
+    "Une intelligence artificielle d’aide à la décision  est élaborée par les pouvoirs publics locaux.",
+    "A 1/2 heure de son activation, l’I.A prends l’initiative d’étendre ses connaissances",
+    "en s’adressant à un public peu mis à contribution dans la recherche de solution : les moins de 18 ans."
+]
+var welcomeSpeech = ['Welcome, Human...']
 
 var speechText = document.createElement('div');
 speechText.className = 'speech-text';
@@ -11,23 +25,26 @@ function iaSpeaking(speech, speechName, specialSpeech) {
     var speechBox = document.getElementById('iaSpeech');
     if (specialSpeech) speechBox = document.getElementById(specialSpeech);
 
-    speechBox.innerHTML = '';
-    // speechBox.appendChild(speechText);
-    // speechBox.appendChild(speechWhiteChar);
+    if (speech.length > 0) {
+        speechBox.innerHTML = '';
+        var line = speech.shift()
+        var letters = line.split("");
+        var writer = setInterval(function () {
+            if (letters.length > 0) {
+                var letter = letters.shift();
+                speechBox.innerHTML += letter;
+            } else {
+                clearInterval(writer);
+                setTimeout(function () {
+                    iaSpeaking(speech, speechName, specialSpeech);
+                }, 2000)
+            }
+        }, 50);
+    } else {
+        endStep(speechName);
+    }
+}
 
-    var splitSpeech = speech.split("");
+function speechLooper() {
 
-    var looper = setInterval(function () {
-        if (splitSpeech.length > 0) {
-            // speechText.style.borderRight = '2px solid white'
-            var letter = splitSpeech.shift();
-
-            // speechText.innerHTML += '|';
-            speechBox.innerHTML += letter;
-            // speechText.appendChild(speechWhiteChar);
-        } else {
-            clearInterval(looper);
-            endStep(speechName);
-        }
-    }, 50);
 }
